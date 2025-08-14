29e9e75ac5326149f5302a60906569d8ebeebb53
yuri-sl <yuri03ysl@gmail.com>
2025-08-14 17:11:06 -0300

    Feat: Implementacao de GET,POST para criarPlayer e listarPlayer

--- src/main/java/io/gith/quarkussocial/rest/PlayerResource.java
+++ src/main/java/io/gith/quarkussocial/rest/PlayerResource.java
@@ -1,14 +1,15 @@
 package io.gith.quarkussocial.rest;
 
-
+import io.gith.quarkussocial.domain.module.User;
+import io.gith.quarkussocial.repositorio.PlayerRepository;
 import io.gith.quarkussocial.rest.dto.CreatePlayerRequest;
-
 import io.gith.quarkussocial.entidade.Player;
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+
+import javax.inject.Inject;
 import javax.transaction.Transactional;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -16,13 +17,21 @@ import javax.ws.rs.core.Response;
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class PlayerResource {
+    @Inject
+    private final PlayerRepository repository;
+    public PlayerResource(PlayerRepository repository){
+        this.repository = repository;
+    }
 
     @POST
     @Transactional
     public Response createPlayer(CreatePlayerRequest playerRequest){
         Player player = new Player();
-
-
-        return  Response.ok(user).build();
+        return  Response.ok(player).build();
+    }
+    @GET
+    public Response listAllPlayers(){
+        PanacheQuery<Player> query = repository.findAll();
+        return Response.ok(query.list()).build();
     }
 }
--- src/main/java/io/gith/quarkussocial/rest/dto/CreatePlayerRequest.java
+++ src/main/java/io/gith/quarkussocial/rest/dto/CreatePlayerRequest.java
@@ -1,4 +1,18 @@
 package io.gith.quarkussocial.rest.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
 public class CreatePlayerRequest {
+    private String player_name;
+    private String player_mail;
+    private LocalDate hire_date;
+    private float experience_points;
 }
